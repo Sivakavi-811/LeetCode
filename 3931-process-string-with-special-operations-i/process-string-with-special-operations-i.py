ce91a5924243630e6646285cd5348b0ebe9a7a88
Sivakavi B <sivakavi0811@gmail.com>
2026-06-16 14:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (98.68%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
@@ -2,12 +2,12 @@ class Solution:
     def processStr(self, s: str) -> str:
         res=""
         for i in range(len(s)):
-            if s[i]>='a'and s[i]<='z':
-                res+=s[i]
-            elif s[i] == '#':
+            if s[i] == '#':
                 res=res*2
             elif s[i] == '%':
                 res=res[::-1]
             elif s[i] == "*":
                 res=res[:-1]
+            else:
+                res+=s[i]
         return res
