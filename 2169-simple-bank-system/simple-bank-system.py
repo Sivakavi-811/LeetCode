a3d99492222af4aa25493fbafe56f6080f902ab0
Sivakavi B <sivakavi0811@gmail.com>
2026-06-19 19:53:52 +0530

    Time: 28 ms (69.23%) | Memory: 49.7 MB (48.95%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.py
+++ 2169-simple-bank-system/simple-bank-system.py
@@ -1,39 +1,39 @@
 class Bank:
 
-    def __init__(self, balance):
-        self.balance = balance
-        self.n = len(balance)
+    def __init__(self, balance: List[int]):
+        self.balance=balance
+        self.n=len(balance)
+    
+    def valid(self, account: int) -> bool:
+        return 1<=account<=self.n
 
-    def valid(self, account):
-        return 1 <= account <= self.n
-
-    def transfer(self, account1, account2, money):
-
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
         if not (self.valid(account1) and self.valid(account2)):
             return False
-
-        if self.balance[account1 - 1] >= money:
-            self.balance[account1 - 1] -= money
-            self.balance[account2 - 1] += money
+        if self.balance[account1-1]>=money:
+            self.balance[account1-1]-=money
+            self.balance[account2-1]+=money
             return True
-
         return False
 
-    def deposit(self, account, money):
-
+    def deposit(self, account: int, money: int) -> bool:
         if self.valid(account):
-            self.balance[account - 1] += money
+            self.balance[account-1]+=money
             return True
-
         return False
 
-    def withdraw(self, account, money):
-
+    def withdraw(self, account: int, money: int) -> bool:
         if not self.valid(account):
             return False
-
-        if self.balance[account - 1] >= money:
-            self.balance[account - 1] -= money
+        if self.balance[account-1]>=money:
+            self.balance[account-1]-=money
             return True
-
         return False
+        
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
