fe15d55fadd1018183938d440763ec4737bdf6d4
Sivakavi B <sivakavi0811@gmail.com>
2026-07-19 17:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (11.32%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.py
+++ 2825-minimize-string-length/minimize-string-length.py
@@ -1,11 +1,4 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
-        seen=[0]*26
-        uni=0
-        for c in s:
-            t=ord(c)-ord('a')
-            if seen[t] == 0:
-                seen[t]=1
-                uni+=1
-        return uni
+        return len(set(s))
         
