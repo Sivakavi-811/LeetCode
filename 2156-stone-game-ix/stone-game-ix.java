0b8486ab2fb4dd10652187658a7033a880784cc9
Sivakavi B <sivakavi0811@gmail.com>
2026-08-16 13:44:20 +0530

    Time: 3 ms (100.00%) | Memory: 114.9 MB (31.76%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.java
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -5,10 +5,7 @@ class Solution {
             cnt[s%3]++;
         }
         if(cnt[0] % 2 == 0){
-            return cnt[1]>0 && cnt[2]>0;
-        }
-        else{
-            return Math.abs(cnt[1]-cnt[2])>2;}
-        
+            return cnt[1]>0 && cnt[2]>0;}
+        return Math.abs(cnt[1]-cnt[2])>2;
     }
 }
