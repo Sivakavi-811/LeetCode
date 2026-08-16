38800c14ee99183399778391f038620cc57a43ca
Sivakavi B <sivakavi0811@gmail.com>
2026-08-16 14:55:15 +0530

    Time: 431 ms (5.02%) | Memory: 273.7 MB (5.00%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> seen = new HashSet<>();
+        HashSet<String> repeated =  new HashSet<>();
+        for(int i=0;i<=s.length()-10;i++){
+            String chunk =  s.substring(i,i+10);
+            if(seen.contains(chunk)){
+                repeated.add(chunk);
+            }
+            else{
+                seen.add(chunk);
+            }
+        }
+        return new ArrayList<>(repeated);
+        
+    }
+}
