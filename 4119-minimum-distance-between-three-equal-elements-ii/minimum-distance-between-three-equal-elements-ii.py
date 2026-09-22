755a4b547d180a6ff353ad55e968c176b055dc40
Sivakavi B <sivakavi0811@gmail.com>
2026-09-22 12:11:22 +0530

    Time: 328 ms (59.66%) | Memory: 50.7 MB (36.65%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -1,17 +1,16 @@
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
         d={}
-        for i,n in enumerate(nums):
-            if n not in d:
-                d[n]=[]
-            d[n].append(i)
-        mdist = 999999
+        for i,num in enumerate(nums):
+            if num not in d:
+                d[num]=[]
+            d[num].append(i)
+        mdist = 9999999999
         for ind in d.values():
-            if len(ind)<3:
+            if(len(ind))<3:
                 continue
             for h in range(len(ind)-2):
-                i = ind[h]
-                j = ind[h+2]
-                mdist = min(mdist, (j - i) * 2)
-        return -1 if mdist == 999999 else mdist
-        
+                i=ind[h]
+                j=ind[h+2]
+                mdist = min(mdist,(j-i)*2)
+        return -1 if mdist == 9999999999 else mdist
